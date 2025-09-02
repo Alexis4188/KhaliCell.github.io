2874ec1760fe91e408047a3e4d0f55d11ccdaa6b
Alexis <126637448+Alexis4188@users.noreply.github.com>
2025-09-01 21:11:23 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const slideElements = document.querySelectorAll(".slide-in");
+    
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible"); // Agrega la clase visible
+            } else{
+                entry.target.classList.remove("visible"); //quita la clase visible
+            }
+        });
+    }, {
+        threshold: 0.2 // El 20% del elemento debe estar visible para activarse
+    });
+    slideElements.forEach(element => observer.observe(element));
+});
+
+document.querySelectorAll('.servicio').forEach(servicio => {
+    servicio.addEventListener('click', () => {
+        servicio.classList.toggle('mostrar-detalles');
+    });
+});
+
+// Botón de subir
+var boton=document.querySelector('.up');
+window.onscroll=function(){
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector('.up').style.display='block';
+    }
+    else{
+        document.querySelector('.up').style.display='none';
+    }
+}
+boton.addEventListener('click', function(){
+    window.scrollTo({top:0, behavior:"smooth"})
+});
+
+//Boton toggle
+var toggle=document.getElementById('contenedor_toggle');
+var body=document.querySelector('body');
+
+toggle.onclick=function(){
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+}
